4448cfcad57194e67b0e828ff80834e5d294bfa3
Ayanna Shaheed <ayanna.shaheed@gmail.com>
2020-10-23 22:05:53 -0400

    Added code to print multiple definitions on different lines

--- dictionary.py
+++ dictionary.py
@@ -7,6 +7,8 @@ data = dict((k.lower(), v) for k, v in data.items())
 word = ""
 
 def define(word):
+
+    word = input("Please enter a word: ")
     word = word.lower()
 
     if word in data:
@@ -17,13 +19,18 @@ def define(word):
         if replace.upper() == "Y":
             return data[get_close_matches(word, data.keys())[0]]
         else:
-            return "Sorry %s doesn't exit in the dictionary" %word
+            return "Sorry %s doesn't exit in the dictionary. Please check your input" %word
     else:
         return "Sorry %s doesn't exit in the dictionary" %word
 
-while word != "%": 
-    word = input("Please enter a word: ")
-    print(define(word))
+while word != "%":
+    result = define(word)
+
+    if type(result) == list:
+        for item in output:
+            print(item)
+    else:
+        print(result)
 
    
   
