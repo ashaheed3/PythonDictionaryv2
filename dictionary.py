bb5bb511f6b709b2bceae8c709b055e3990b4cf8
Ayanna Shaheed <ayanna.shaheed@gmail.com>
2020-10-23 21:45:46 -0400

    Added closest match functionality

--- dictionary.py
+++ dictionary.py
@@ -1,5 +1,5 @@
 import json
-import difflib
+from difflib import get_close_matches
 
 data = json.load(open("data.json"))
 data = dict((k.lower(), v) for k, v in data.items())
@@ -11,6 +11,13 @@ def define(word):
 
     if word in data:
         return data[word]
+    elif len(get_close_matches(word, data.keys())) > 0:
+        replace = input("Sorry %s doesn't exist in the dictionary. Did you mean %s instead? [Y/N] " % (word, get_close_matches(word, data.keys())[0]))
+              
+        if replace.upper() == "Y":
+            return data[get_close_matches(word, data.keys())[0]]
+        else:
+            return "Sorry %s doesn't exit in the dictionary" %word
     else:
         return "Sorry %s doesn't exit in the dictionary" %word
 
